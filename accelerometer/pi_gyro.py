79eab2989cc8d5609212e13ac0951fc808e9552e
byapparov <byapparov@gmail.com>
2021-01-09 16:29:15 +0000

    replaced output with gyro data

--- accelerometer/pi_gyro.py
+++ accelerometer/pi_gyro.py
@@ -6,10 +6,12 @@ from mpu6050 import mpu6050
 sensor = mpu6050(0x68)
 
 accelerometer_data = sensor.get_accel_data()
+gyro_data = sensor.get_gyro_data()
+
 
 
 print("X: {0}, Y: {1}, Z: {2}".format(
-    accelerometer_data['x'],
-    accelerometer_data['y'],
-    accelerometer_data['z']
+    gyro_data['x'],
+    gyro_data['y'],
+    gyro_data['z']
 ))
