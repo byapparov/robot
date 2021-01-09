b05eefbb07deae02ac9f0addf9a368b809fc1b18
byapparov <byapparov@gmail.com>
2021-01-09 16:44:36 +0000

    Add integration of gyro data

--- accelerometer/pi_gyro.py
+++ accelerometer/pi_gyro.py
@@ -8,10 +8,29 @@ sensor = mpu6050(0x68)
 accelerometer_data = sensor.get_accel_data()
 gyro_data = sensor.get_gyro_data()
 
+gyro_angles = {
+    'x': 0,
+    'y': 0,
+    'z': 0
+}
 
+def loop():
 
-print("X: {0}, Y: {1}, Z: {2}".format(
-    gyro_data['x'],
-    gyro_data['y'],
-    gyro_data['z']
-))
+    dt = 0.1
+    while(True):
+
+    print("X: {0}, Y: {1}, Z: {2}".format(
+        gyro_data['x'],
+        gyro_data['y'],
+        gyro_data['z']
+    ))
+
+    gyro_angles['x'] += gyro[0]/131.0 * dt
+    gyro_angles['y'] += gyro[1]/131.0 * dt
+    gyro_angles['z'] += gyro[2]/131.0 * dt
+
+    print("total X: {0}, Y: {1}, Z: {2}".format(
+        gyro_angles['x'],
+        gyro_angles['y'],
+        gyro_angles['z']
+    ))
