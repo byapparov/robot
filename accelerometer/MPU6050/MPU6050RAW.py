7418a50d0f90d8fe2b22f261443d7cb42ed46425
byapparov <byapparov@gmail.com>
2021-01-09 16:14:32 +0000

    Added integration of angle changes to track the total angle

--- accelerometer/MPU6050/MPU6050RAW.py
+++ accelerometer/MPU6050/MPU6050RAW.py
@@ -25,9 +25,16 @@ def single_measurement():
         gyro[2]/131.0
     ))
 
+gyro_angles = {
+    x: 0,
+    y: 0,
+    z: 0
+}
 def loop():
 
+    dt = 0.1
     while(True):
+
         accel = mpu.get_acceleration()      # get accelerometer data
         gyro = mpu.get_rotation()           # get gyroscope data
         # print("a/g:%d\t%d\t%d\t%d\t%d\t%d "%(accel[0],accel[1],accel[2],gyro[0],gyro[1],gyro[2]))
@@ -39,7 +46,17 @@ def loop():
             gyro[2]/131.0
         ))
 
-        time.sleep(0.1)
+        gyro_angles["x"] += gyro[0]/131.0 * dt
+        gyro_angles["y"] += gyro[1]/131.0 * dt
+        gyro_angles["z"] += gyro[2]/131.0 * dt
+
+        print("total X: {0}, Y: {1}, Z: {2}".format(
+            gyro_angles["x"],
+            gyro_angles["y"],
+            gyro_angles["z"]
+        ))
+
+        time.sleep(dt)
 
 if __name__ == '__main__':     # Program entrance
     print("Program is starting ... ")
