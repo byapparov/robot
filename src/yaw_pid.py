b9a78be602175c35543f706ec47b44905d52d0d2
byapparov <byapparov@gmail.com>
2021-01-10 14:48:30 +0000

    Complete renaming of the variable

--- src/yaw_pid.py
+++ src/yaw_pid.py
@@ -8,7 +8,7 @@ class YawControl:
 
     def __init__(self, kp, ki, kd):
 
-        self.int_val = 0.0
+
         self.last_error = 0.0
         self.integral = 0.0
 
@@ -22,10 +22,10 @@ class YawControl:
 
 
     def step(self, error, sample_time):
-        self.integral = self.int_val + error * sample_time;
+        self.integral = self.integral + error * sample_time;
         derivative = (error - self.last_error) / sample_time;
 
-        control = self.kp * error + self.ki * integral + self.kd * derivative;
+        control = self.kp * error + self.ki * self.integral + self.kd * derivative;
         if abs(control) < MIN_MOTOR_CONTROL:
             control = math.copysign(control, MIN_MOTOR_CONTROL)
         self.last_error = error
