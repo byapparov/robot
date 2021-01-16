06da786b47780a2c407884aeb6493c85f3edaf37
byapparov <byapparov@gmail.com>
2021-01-16 13:22:07 +0000

    Added self

--- src/ranging.py
+++ src/ranging.py
@@ -5,5 +5,5 @@ class UltrasonicSensor:
     def __init__(self, trigger_pin = 23, echo_pin = 24):
         sensor = DistanceSensor(trigger_pin, echo_pin)
 
-    def distance():     # get the measurement results of ultrasonic module,with unit: cm
+    def distance(self):     # get the measurement results of ultrasonic module,with unit: cm
         sensor.distance
