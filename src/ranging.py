d829d5218a869836f45392bd0d8e0bdde6012245
byapparov <byapparov@gmail.com>
2021-01-16 13:35:09 +0000

    align pin order in calls

--- src/ranging.py
+++ src/ranging.py
@@ -2,8 +2,8 @@ from gpiozero import DistanceSensor
 
 class UltrasonicSensor:
 
-    def __init__(self, trigger_pin = 23, echo_pin = 24):
-        self.sensor = DistanceSensor(trigger_pin, echo_pin)
+    def __init__(self, echo_pin = 23, trigger_pin = 24):
+        self.sensor = DistanceSensor(echo_pin, trigger_pin)
 
     def distance(self):     # get the measurement results of ultrasonic module,with unit: cm
         self.sensor.distance
