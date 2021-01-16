0f8b64cbc3aa2bd5ce99b2ac787c0a102d3b1aa5
byapparov <byapparov@gmail.com>
2021-01-16 13:25:51 +0000

    Added self as ref

--- src/ranging.py
+++ src/ranging.py
@@ -3,7 +3,7 @@ from gpiozero import DistanceSensor
 class UltrasonicSensor:
 
     def __init__(self, trigger_pin = 23, echo_pin = 24):
-        sensor = DistanceSensor(trigger_pin, echo_pin)
+        self.sensor = DistanceSensor(trigger_pin, echo_pin)
 
     def distance(self):     # get the measurement results of ultrasonic module,with unit: cm
-        sensor.distance
+        self.sensor.distance
