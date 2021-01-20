c0ee27e47702e8982e59c79932978d8be6f5ef95
byapparov <byapparov@gmail.com>
2021-01-20 00:11:16 +0000

    Updated pins to the working combo

--- experiments/gpiozero_distance.py
+++ experiments/gpiozero_distance.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(23, 24)
+sensor = DistanceSensor(24, 23)
 
 while True:
     print('Distance to nearest object is', sensor.distance, 'm')
