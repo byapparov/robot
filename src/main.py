aa8166fe66b46ed1ab1eb618b1e2b9ada333ee41
byapparov <byapparov@gmail.com>
2021-01-23 12:27:38 +0000

    increase stop to 40cm

--- src/main.py
+++ src/main.py
@@ -9,8 +9,8 @@ def main():
         forward_space = robot.forward_distance()
         if forward_space > 1:
             robot.move(0.5, 0)
-        elif forward_space > 0.2:
-            robot.move(forward_space - 0.25, 0)
+        elif forward_space > 0.4:
+            robot.move(forward_space - 0.45, 0)
         else:
             robot.move(-0.1, 0.1)
 
