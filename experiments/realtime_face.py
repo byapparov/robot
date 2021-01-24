a4e1d8baf9cabad281f1416d8979c15aa7e328bc
byapparov <byapparov@gmail.com>
2021-01-24 23:42:05 +0000

    missing object

--- experiments/realtime_face.py
+++ experiments/realtime_face.py
@@ -42,6 +42,7 @@ while True:
     ret, frame = video_capture.read()
 
     # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = frame
     # small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
