7ac13fd9a1d8783f5cb3421f4a80250850c64dce
byapparov <byapparov@gmail.com>
2021-01-24 23:56:01 +0000

    Added debugging

--- experiments/realtime_face.py
+++ experiments/realtime_face.py
@@ -66,6 +66,7 @@ while True:
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            print("Found face matches: {n}".format(n = len(matches)))
             name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
