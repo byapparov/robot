7c5fc6686a4a22d2d2d4b0f9708db283b217a437
byapparov <byapparov@gmail.com>
2021-01-25 00:03:33 +0000

    Change scale of small frame

--- experiments/realtime_face.py
+++ experiments/realtime_face.py
@@ -42,6 +42,8 @@ start_time = time.time()
 
 fps_limit = 1
 
+optimal_scale = 0.5
+
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
@@ -51,7 +53,7 @@ while True:
         continue
 
     # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    small_frame = cv2.resize(frame, (0, 0), fx=optimal_scale, fy=optimal_scale)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
@@ -89,10 +91,10 @@ while True:
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        top *= 1 / optimal_scale
+        right *= 1 / optimal_scale
+        bottom *= 1 / optimal_scale
+        left *= 1 / optimal_scale
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
